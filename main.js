0c8de0c8ca4c5b1916314f6372bba394dae3aabe
nickpatriotthepar0tdoyouseeme <157007445+nickpatriotthepar0tdoyouseeme@users.noreply.github.com>
2024-08-15 08:35:17 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+import { Talk } from "talkjs";
+
+await Talk.ready;
+
+const me = new Talk.User("sample_user_alice");
+const session = new Talk.Session({ appId: "tyzQEqyO", me });
+
+const chatbox = session.createChatbox();
+chatbox.select("sample_conversation");
+chatbox.mount(document.getElementById("talkjs-container"));
+
+
+
+
+
+
+:npm install talkjs @talkjs/react
